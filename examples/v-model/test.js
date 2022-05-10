2bbadae2f1e5e51fafb8d2d845e61d45781e455c
“ <wangjy3@wedoctor.com>
2022-05-10 21:16:58 +0800

    v-if-for

--- examples/v-model/test.js
+++ examples/v-model/test.js
@@ -3,8 +3,10 @@ let compiler = require("vue-template-compiler");
 // const ast1 = compiler.compile("<div v-if='false'></div>");
 // console.log(ast1.render);
 const ast2 = compiler.compile(
-  "<div v-for='(item,index) in 3' :key='index'></div>"
+  "<div v-for='(item,index) in 3' :key='index' v-if='false'></div>"
 );
-console.log(ast2.render());
+console.log(ast2.render);
+// const ast3 = compiler.compile("<input type='text' v-model='name'");
+// console.log(ast3.render());
 // const ast3 = compiler.compile("<input type='text' v-model='name'");
 // console.log(ast3.render());

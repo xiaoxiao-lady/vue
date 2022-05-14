8d13923069c33f0c2c7ff55a0aa2055d7b13e0a4
wangjy3 <wangjy3@wedoctor.com>
2022-05-14 17:14:30 +0800

    vue3

--- my-vue3/src/index.js
+++ my-vue3/src/index.js
@@ -1,9 +1,19 @@
 import { defineProxy } from "./proxy-demo.js";
 import { defineReactive } from "./defineProperty-demo";
-const obj = {
+const data = {
   a: 1,
+  b: {
+    c: {
+      d:{
+        e:1
+      }
+    },
+  },
 };
-defineProxy(obj);
-Object.keys().forEach((key) => defineReactive(obj, key, obj[key]));
-obj.a = 3;
-// const arr = [1, 2, 3];
+// const data = [1, 2, 3];
+const proxyData = defineProxy(data); //想要proxy监听到改变，一定要定义一个变量，操作这个变量proxyData
+// Object.keys(data).forEach((key) => defineReactive(data, key, data[key]));
+// obj.a = 3; //会触发Object.definePropety
+// proxyData.a = 3; //会触发Object.definePropety和proxy
+console.log(proxyData.b)
+// proxyData.push(4);
